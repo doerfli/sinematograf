2dccfae33ad1d1e915023e29293b3bb9a07c457e
Marc Doerflinger <marc@doerf.li>
2025-03-01 20:27:19 +0000

    Add PostgreSQL test container support and related tests

---
+++ src/test/java/li/doerf/sinematograf/cinema/PostgresDBTestResource.java
@@ -0,0 +1,34 @@
+package li.doerf.sinematograf.cinema;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import io.quarkus.logging.Log;
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+
+public class PostgresDBTestResource implements QuarkusTestResourceLifecycleManager {
+    
+    private PostgreSQLContainer postgres;
+
+    @Override
+    public Map<String, String> start() {
+        postgres = new PostgreSQLContainer<>("postgres:17-alpine");
+        postgres.start();
+        Log.info("started postgres container");
+        Map<String, String> conf = new HashMap<>();
+        conf.put("quarkus.datasource.jdbc.url", postgres.getJdbcUrl());
+        conf.put("quarkus.datasource.username", postgres.getUsername());
+        conf.put("quarkus.datasource.password", postgres.getPassword());
+        conf.put("quarkus.datasource.reactive.url", postgres.getJdbcUrl().replace("jdbc:",  ""));
+        return conf;
+    }
+
+    @Override
+    public void stop() {
+        postgres.stop();
+        Log.info("stopped postgres container");
+    }
+
+}
---
+++ src/test/java/li/doerf/sinematograf/cinema/resources/CinemaResourceTest.java
@@ -0,0 +1,56 @@
+package li.doerf.sinematograf.cinema.resources;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.CoreMatchers.containsString;
+
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import li.doerf.sinematograf.cinema.PostgresDBTestResource;
+
+@QuarkusTest
+@TestHTTPEndpoint(CinemaResource.class)
+@QuarkusTestResource(value = PostgresDBTestResource.class, restrictToAnnotatedClass = true)
+class CinemaResourceTest {
+
+
+    @Test
+    void testCreateEndpoint() {
+        var cinemaName = "Cinema%f".formatted(new Random().nextDouble());
+        // given
+        given()
+            .contentType("application/json")
+            .body(
+                """
+                {
+                    "name": "%s",
+                    "street": "Street",
+                    "zip": "12345",
+                    "city": "City"
+                }
+                """.formatted(cinemaName)
+            )
+        .when()
+            .post()
+        // then
+        .then()
+            .statusCode(200);
+
+        await().atMost(5, SECONDS).until(() -> {
+            when()
+                .get()
+            .then()
+                .statusCode(200)
+                .body(containsString(cinemaName));
+            return true;
+        });
+    }
+
+}
